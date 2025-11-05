3a64b9d2fe9d34833d8934cd3e6fcca2edfa14e9
Nhat Truong <tntruong196@gmail.com>
2025-11-05 20:17:56 +0700

    update temp

--- SVG_DEMO/SVG Source Code/Circle.cpp
+++ SVG_DEMO/SVG Source Code/Circle.cpp
@@ -15,7 +15,8 @@ float SVGCircle::getR() const {
 
 void SVGCircle::parseAttributes(xml_node<>* Node)
 {
-	GeometricElement::parseAttributes(Node);
+	//GeometricElement::parseAttributes(Node);
+	SVGEllipse::parseAttributes(Node);
 	if (xml_attribute<>* attribute = Node->first_attribute("r"))
 	{
 		this->setR((atof(attribute->value())));
@@ -25,7 +26,15 @@ void SVGCircle::parseAttributes(xml_node<>* Node)
 
 void SVGCircle::draw()
 {
-
+	Color fillColor = { getFill().getA(),getFill().getR(),getFill().getG(),getFill().getB()};
+	SolidBrush brush(fillColor);
+	Color fillColorWidth = { getStroke().getColor().getA(),getStroke().getColor().getR() ,getStroke().getColor().getG() ,getStroke().getColor().getB() };
+	Pen pen = { fillColorWidth,getStroke().getWidth()};
+	float diameter = 2 * r;
+	float x = getRx() - r;
+	float y = getRy() - r;
+	graphics->FillEllipse(&brush, x, y, diameter, diameter);
+	graphics->DrawEllipse(&pen, x, y, diameter, diameter);
 }
 
 SVGCircle::~SVGCircle()
