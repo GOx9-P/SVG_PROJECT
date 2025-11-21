d351c0675ad4e2d2cceba82e4974f12700ecb51b
Pham Minh Trong <leom10wc22@gmail.com>
2025-11-21 09:00:15 +0700

    Build SVGGroup.h and SVGPath.h

---
+++ SVG_DEMO/SVG Source Code/SVGGroup.h
@@ -0,0 +1,20 @@
+#ifndef _SVGGROUP_H_
+#define _SVGGROUP_H_
+
+#include "SVGElement.h"
+
+class SVGGroup : public SVGElement {
+private:
+	vector<SVGElement*> group;
+public:
+	SVGGroup();
+	SVGGroup(const SVGGroup&);
+	SVGGroup(const vector<SVGElement*>);
+	void parseAttributes(xml_node<>* node);
+	void draw(Graphics* graphics);
+	void addElement(SVGElement* element); //Them 1 con vao nhom
+	~SVGGroup();
+};
+
+#endif
+
